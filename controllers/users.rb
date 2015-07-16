cf875c91909d0ab425603cd69a33468e83c97902
Cameron Helkuik <cameronhelkuik@gmail.com>
2015-07-15 19:49:13 -0500

    Verify password log in works, need to debug sign up

--- controllers/users.rb
+++ controllers/users.rb
@@ -15,17 +15,16 @@ end
 
 get "/verify_login" do
   attempted_password = params["password"]
-  user = User.search_row("email", params["email"])
-
+  user = User.search_rows("email", params["email"])
   # Assuming there is a user with the given email address...
   # Make a new BCrypt object with the **password from the database**.
-  actual_password = BCrypt::Password.new(user.password)
-
+  actual_password = BCrypt::Password.new(user[0].password)
+  
   # So, an example:
   # actual_password = BCrypt::Password.new("$2a$10$87jFZs7pY2Fh33HR.lA9ouVLzevh45esv0UjdYF/b1jOGKC.YtfG2")
 
   if actual_password == attempted_password
-    erb :"password_success"
+    erb :"/login/password_success"
   else
     erb :"/login/login_form"
   end
